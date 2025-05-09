1446488c0f99f6228bde5d1dc988b44cb9f65547
Alex Silva <alexooodev@gmail.com>
2025-05-09 20:30:12 +0100

    reorder main page and fix typescript version issue

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,13 @@
-import React from 'react';
-import Navbar from './components/Navbar';
-import Sidebar from './components/Sidebar';
-import Hero from './components/Hero';
-import WorkExperience from './components/WorkExperience';
-import ProjectsSection from './components/ProjectsSection';
-import AboutMe from './components/AboutMe';
-import Footer from './components/Footer';
-import ExperienceModal from './components/ExperienceModal';
-import { useSidebarStore } from './store/sidebarStore';
+import React from "react";
+import Navbar from "./components/Navbar";
+import Sidebar from "./components/Sidebar";
+import Hero from "./components/Hero";
+import WorkExperience from "./components/WorkExperience";
+import ProjectsSection from "./components/ProjectsSection";
+import AboutMe from "./components/AboutMe";
+import Footer from "./components/Footer";
+import ExperienceModal from "./components/ExperienceModal";
+import { useSidebarStore } from "./store/sidebarStore";
 
 function App() {
   const { isSidebarOpen } = useSidebarStore();
@@ -17,14 +17,14 @@ function App() {
       <Navbar />
       <Sidebar />
       <ExperienceModal />
-      
-      <main className={`flex-grow transition-all duration-300 ${isSidebarOpen ? 'md:ml-64' : 'ml-0'}`}>
+
+      <main className={`flex-grow transition-all duration-300 ${isSidebarOpen ? "md:ml-64" : "ml-0"}`}>
         <Hero />
-        <WorkExperience />
         <ProjectsSection />
+        <WorkExperience />
         <AboutMe />
       </main>
-      
+
       <Footer />
     </div>
   );
